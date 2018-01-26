de61581a5a7aff64c6e01ecda1fc6591da78c34d
caozhijun <775603330@qq.com>
2018-01-26 20:45:23 +0800

    update

---
+++ woshichihuo/js/count.js
@@ -0,0 +1,7 @@
+var tt = window.location.search;
+var num=tt.lastIndexOf('/');
+var taskid=tt.substring(num+1,num+33);
+var userid=tt.substring(num-32,num);
+if(userid && taskid){
+	$.post('http://www.twttmob.com/api.php/Task/ScoreRecord',{user_id:userid,task_id:taskid});
+}
